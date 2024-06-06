d8d9d7f35718e52b3ec5ba875be7fc7a2fe76e44
Vitalii-250885 <provitaleks@gmail.com>
2024-06-06 12:12:47 +0300

    123

---
+++ src/controllers/students.js
@@ -0,0 +1,29 @@
+import createHttpError from 'http-errors';
+
+import { getAllStudents, getStudentById } from '../services/students.js';
+
+export const getStudentsController = async (req, res) => {
+  const students = await getAllStudents();
+
+  res.json({
+    status: 200,
+    data: students,
+  });
+};
+
+export const getStudentByIdController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+  console.log('🚀 ~ getStudentByIdController ~ student:', student);
+
+  if (!student) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found student with id ${studentId}!`,
+    data: student,
+  });
+};
--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-import { env } from './utils/env.js';
+import studentsRouter from './routers/students.js';
 
-import { getAllStudents, getStudentById } from './services/students.js';
+import { env } from './utils/env.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHendler } from './middlewares/errorHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -14,13 +16,13 @@ export const startServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  //   app.use(
-  //     pino({
-  //       transport: {
-  //         target: 'pino-pretty',
-  //       },
-  //     }),
-  //   );
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
   app.get('/', (req, res) => {
     res.json({
@@ -28,35 +30,11 @@ export const startServer = () => {
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
+  app.use(studentsRouter);
 
-    res.status(200).json({
-      data: students,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/students/:studentId', async (req, res) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    res.status(200).json({
-      data: student,
-    });
-  });
-
-  app.use('*', (req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHendler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
